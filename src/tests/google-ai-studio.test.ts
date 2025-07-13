5fc61f151a022ba90636a8f3cfb9c86133a4fdba
Zhu Liang <zhuliangg11@gmail.com>
2025-07-13 17:00:10 +0800

    feat: fix google ai test

--- src/tests/google-ai-studio.test.ts
+++ src/tests/google-ai-studio.test.ts
@@ -2,7 +2,7 @@ import { sendPrompt } from "../index";
 import { AI_PROVIDERS, ModelEnum } from "llm-info";
 
 // const googleModel = ModelEnum["gemini-2.5-pro-exp-03-25"];
-const googleModel = ModelEnum["gemini-2.5-flash-preview-04-17"];
+const googleModel = ModelEnum["gemini-2.5-flash"];
 
 describe("Google Provider", () => {
   // Skip test if GEMINI_API_KEY is not set
